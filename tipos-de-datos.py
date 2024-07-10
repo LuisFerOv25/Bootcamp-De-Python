9d4e1a837ce3443e6618ee2217d65605fe75e33d
Luis Oviedo <ingluisfer25@udenar.edu.co>
2024-07-10 18:54:51 -0500

    Se inicializa el repositorio de Python

---
+++ tipos-de-datos.py
@@ -0,0 +1,12 @@
+"string"
+'string'
+'''string'''
+"""string"""
+#tipos de datos numericos
+4#tipo de datos entero
+4.5#tipo de dato flotante
+
+
+#tipos de datos booleanos
+True#True va con T mayúscula
+False#False va con F mayúscula

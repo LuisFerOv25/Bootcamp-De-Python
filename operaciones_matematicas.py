0d0bae5cbf5d2f33f0c8b080f926226c83ff06e6
Luis Oviedo <ingluisfer25@udenar.edu.co>
2024-07-17 20:47:38 -0500

    Aborda Operaciones matematicas V 2.5

---
+++ operaciones_matematicas.py
@@ -0,0 +1,36 @@
+#Operaciones Matematicas
+a = 5
+b = 3
+print(a> b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+print(f'Suma: {a+b}')
+print(f'Resta: {a-b}')
+print(f'Multiplicacion: {a*b}')
+print(f'Division: {a/b}')
+# En python el operador modulo  (%) obtiene el resto de una division
+print(f'Resto: {a%b}')
+#El siguiente signo es el doble division, dvision entera
+print(f'Division Entera: {a//b}')
+#Potencias
+print(f'Potencia: {a**b}')
+#artificio
+a +=2
+print(a)
+a *=2
+print(a)
+a /=2
+print(a)
+
+operacion_1 = 2 + 3 * 4
+print(operacion_1)
+operacion_2 = 2 + (3*4)
+print(operacion_2)
+operacion_3 = (2+3)*4
+print(operacion_3)
+operacion_4 = (2 + 3) * (4 ** 2) / 8 -1
+print(operacion_4)
